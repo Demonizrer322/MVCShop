0bb6013c60931ba256e0a34942491fd555fcd367
Demonizrer322 <dimond.spets@gmail.com>
2019-09-17 22:58:18 +0300

    cart QuantityOfProduct input

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,31 +9,31 @@ $(function(){
         },
         function (data){
             let product = JSON.parse(data);
+            console.log(data);
             $.each(product, function(i, v) {
+                console.log(v.Id);
+                console.log(v);
                 var html ='';
                 product.pop();
-                console.log(v);
-                console.log(v['MainImage']);
-                console.log(v['Id']);
                 html += `<div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="single-grid-product mb-40">
                                 <div class="product-image">
                                     <!-- <div class="product-label">
                                         <span>-20%</span>
                                     </div> -->
-                                    <a id="hrefsingleProduct" href="/product/singleProductTabstyle3?IdProduct=${v['Id']}">
-                                        <img src="${v['MainImage']}" class="main-image" alt=""></a>
+                                    <a id="hrefsingleProduct" href="/product/singleProductTabstyle3?IdProduct=${v.Id}">
+                                        <img src="${v.MainImage}" class="main-image" alt=""></a>
                                     <div class="product-action">
                                         <ul>
-                                            <li class="addingToCart" data-id="${v['Id']}"><a href="#"><i class="fa fa-cart-plus"></i></a></li>
-                                            <li class="openModal" data-id="${v['Id']}"><a href="#quick-view-modal-container" data-toggle="modal" title="Quick View"><i class="fa fa-eye"></i></a></li>
+                                            <li class="addingToCart" data-id="${v.Id}"><a href="#"><i class="fa fa-cart-plus"></i></a></li>
+                                            <li class="openModal" data-id="${v.Id}"><a href="#quick-view-modal-container" data-toggle="modal" title="Quick View"><i class="fa fa-eye"></i></a></li>
                                             <li><a href="wishlit.php"><i class="fa fa-heart-o"></i></a></li>
                                         </ul>
                                     </div>
                                 </div>
                                 <div class="product-content">
-                                    <h3 class="title"> <a href="/product/singleProductTabstyle3?IdProduct=${v['Id']}">${v['Name']}</a></h3>
-                                    <p class="product-price"><!--<span class="discounted-price">$</span>--> <span class="main-price">$${v['Price']}</span></p>
+                                    <h3 class="title"> <a href="/product/singleProductTabstyle3?IdProduct=${v.Id}">${v.Name}</a></h3>
+                                    <p class="product-price"><!--<span class="discounted-price">$</span>--> <span class="main-price">$${v.Price}</span></p>
                                 </div>
                             </div>
                         </div>`;
@@ -43,6 +43,7 @@ $(function(){
             console.log(product[0]);
         });
     });
+
     $(".openModal").click(function(){
         var Id = $(this).attr('data-id');
         console.log(Id);
@@ -63,27 +64,34 @@ $(function(){
         console.log(IdProduct);
         $.get("/cart/addingToCart?IdProduct=" + IdProduct);
     });
-    $price = $('.priceValue').val();
-    console.log($price);
-    $('.quantity-info').on('input', function(event) {
-        let target = $(event.target);
-        if(target.is(".quantity-info"))
-        {
-            target.children().toggle();
+        
+
+        var arrayBtnQuantity = $('.qtybtn');
+        var lenghtBtnQuantity = arrayBtnQuantity.length;
+        for (var i = 0; i < lenghtBtnQuantity; i++){
+            $(arrayBtnQuantity[i]).click(function(event) {
+                var element = event.srcElement || event.target || event.toElement;
+                var parent = $(element).parent();
+                var QuantityInput = $(parent).children('.quantity-info-input');
+                let valueOfInput = $(QuantityInput).val();
+                console.log(valueOfInput);
+                let price = $('.priceValue').val();
+                console.log(price);
+            });
         }
-        $valueOfInput = $('.quantity-info-input').val();
-        console.log($valueOfInput);
-        $sum = $valueOfInput * $price;
-        $('.totalPrice').empty();
-        $sumArea = $('.totalPrice').text("$" + $sum);
-        console.log($sum);
-    });
-    $('.quantity-info').click(function(event) {
-        $valueOfInput = $('.quantity-info-input').val();
-        console.log($valueOfInput);
-        $sum = $valueOfInput * $price;
-        $('.totalPrice').empty();
-        $sumArea = $('.totalPrice').text("$" + $sum);
-        console.log($sum);
-    });
+        var arrayInputQuantity = $('.quantity-info');
+        var lenghtInputQuantity = arrayInputQuantity.length;
+        for (var i = 0; i < lenghtInputQuantity; i++)
+        {
+            $(arrayInputQuantity[i]).on('input', function(event) {
+                var element = event.srcElement || event.target || event.toElement;
+                var parent = $(element).parent();
+                var QuantityInput = $(parent).children('.quantity-info-input');
+                $valueOfInput = $(QuantityInput).val();
+                console.log($valueOfInput);
+                var priceValue = $('.priceValue');
+                let price = $(priceValue[i]).val();
+                console.log(priceValue);
+            });
+        }
 });
